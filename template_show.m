8321ef053bb9e34487f168459569d615cb3c04b5
prachijagad <122281108+prachijagad@users.noreply.github.com>
2024-02-04 23:39:29 +0530

    Add files via upload

---
+++ template_show.m
@@ -0,0 +1,21 @@
+A=imread('alpha/A.bmp');B=imread('alpha/B.bmp');C=imread('alpha/C.bmp');
+D=imread('alpha/D.bmp');E=imread('alpha/E.bmp');F=imread('alpha/F.bmp');
+figure;
+subplot 231
+imshow(A)
+title("A")
+subplot 232
+imshow(B)
+title("B")
+subplot 233
+imshow(C)
+title("C")
+subplot 234
+imshow(D)
+title("D")
+subplot 235
+imshow(E)
+title("E")
+subplot 236
+imshow(F)
+title("F")
